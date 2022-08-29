b2f6bd7fb75cb9527540b0cf91e3308646ed08ec
classyrazy <classyrazy014@gmail.com>
2022-08-29 13:07:32 +0100

    Fixing vercel error

--- plugins/vueModally.ts
+++
@@ -1,6 +0,0 @@
-import { defineNuxtPlugin } from "#app";
-import vueModallyV3 from "vue-modally-v3";
-
-export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.use(vueModallyV3, {});
-});
